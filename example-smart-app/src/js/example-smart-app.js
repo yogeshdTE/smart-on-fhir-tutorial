e1bc0e3eed06ff8d3af06fb2c5959c9366a4be5c
yogeshdTE <98949235+yogeshdTE@users.noreply.github.com>
2022-02-24 16:28:08 +0530

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -11,21 +11,21 @@
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
-        var obv = smart.patient.api.fetchAll({
-                    type: 'Observation',
-                    query: {
-                      code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-                      }
-                    }
-                  });
+        //var obv = smart.patient.api.fetchAll({
+        //            type: 'Observation',
+        //            query: {
+        //              code: {
+        //                $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+        //                      'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
+        //                      'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+        //              }
+        //            }
+        //          });
 
-        $.when(pt, obv).fail(onError);
+        $.when(pt/*, obv*/).fail(onError);
 
-        $.when(pt, obv).done(function(patient, obv) {
-          var byCodes = smart.byCodes(obv, 'code');
+        $.when(pt/*, obv*/).done(function(patient/*, obv*/) {
+          //var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
 
           var fname = '';
@@ -36,29 +36,29 @@
             lname = patient.name[0].family.join(' ');
           }
 
-          var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-          var hdl = byCodes('2085-9');
-          var ldl = byCodes('2089-1');
+          //var height = byCodes('8302-2');
+          /var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
+          //var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
+          //var hdl = byCodes('2085-9');
+          //var ldl = byCodes('2089-1');
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
           p.gender = gender;
           p.fname = fname;
           p.lname = lname;
-          p.height = getQuantityValueAndUnit(height[0]);
+          ///p.height = getQuantityValueAndUnit(height[0]);
 
-          if (typeof systolicbp != 'undefined')  {
-            p.systolicbp = systolicbp;
-          }
+          //if (typeof systolicbp != 'undefined')  {
+          //  p.systolicbp = systolicbp;
+          //}
 
-          if (typeof diastolicbp != 'undefined') {
-            p.diastolicbp = diastolicbp;
-          }
+          //if (typeof diastolicbp != 'undefined') {
+          //  p.diastolicbp = diastolicbp;
+          //}
 
-          p.hdl = getQuantityValueAndUnit(hdl[0]);
-          p.ldl = getQuantityValueAndUnit(ldl[0]);
+          //p.hdl = getQuantityValueAndUnit(hdl[0]);
+         // p.ldl = getQuantityValueAndUnit(ldl[0]);
 
           ret.resolve(p);
         });
@@ -78,11 +78,11 @@
       lname: {value: ''},
       gender: {value: ''},
       birthdate: {value: ''},
-      height: {value: ''},
-      systolicbp: {value: ''},
-      diastolicbp: {value: ''},
-      ldl: {value: ''},
-      hdl: {value: ''},
+      //height: {value: ''},
+      //systolicbp: {value: ''},
+      //diastolicbp: {value: ''},
+      //ldl: {value: ''},
+      //hdl: {value: ''},
     };
   }
 
